a0a038408243e3169f632851bd5e8cbe1e77adf2
Akash Kumar <akashkumar@Akashs-MacBook-Pro.local>
2021-01-13 23:27:34 -0500

    tanzim's pipeline

--- pageAssembly.js
+++ pageAssembly.js
@@ -4,11 +4,11 @@ const ejs = require('ejs');
 
 //micro pages
 let microPages = JSON.parse(fs.readFileSync('data/data.json', 'utf8'));
-let micro_template = fs.readFileSync('views/micro.ejs', 'utf8');
+let micro_template = fs.readFileSync('src/views/micro.ejs', 'utf8');
 
 for (let i = 0; i < microPages.length; i++) {
   let micro_html = ejs.render(micro_template, {//loop through each blog post
-    filename: __dirname + '/views/micro.ejs',
+    filename: __dirname + '/src/views/micro.ejs',
     page: microPages[i],
     allPages: microPages
   });
@@ -18,9 +18,9 @@ for (let i = 0; i < microPages.length; i++) {
 
 
 // macro
-let macro_template = fs.readFileSync('views/macro.ejs', 'utf8');
+let macro_template = fs.readFileSync('src/views/macro.ejs', 'utf8');
 let macro_html = ejs.render(macro_template, {
-  filename: __dirname + '/views/about.ejs',
+  filename: __dirname + '/src/views/about.ejs',
   allPages: microPages
 });
 fs.writeFileSync("build/index.html", macro_html, 'utf8');
@@ -28,9 +28,9 @@ fs.writeFileSync("build/index.html", macro_html, 'utf8');
 
 
 // about
-let about_template = fs.readFileSync('views/about.ejs', 'utf8');
+let about_template = fs.readFileSync('src/views/about.ejs', 'utf8');
 let about_html = ejs.render(about_template, {
-  filename: __dirname + '/views/about.ejs',
+  filename: __dirname + '/src/views/about.ejs',
   allPages: microPages
 });
 fs.writeFileSync("build/about.html", about_html, 'utf8');
--- util/precompile.js
+++ util/precompile.js
@@ -1,24 +1,92 @@
+//deletes contents of /build and /dist
+//creates necessary subfolders
+//copy elements from /src/public to /build (images, scripts, etc.)
+
 const fs = require('fs');
 
-function deleteFolderRecursive(path) {
-  if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
-    fs.readdirSync(path).forEach(function(file, index){
-      var curPath = path + "/" + file;
 
-      if (fs.lstatSync(curPath).isDirectory()) { // recurse
-        deleteFolderRecursive(curPath);
-      } else { // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
+const build = '../build/';
+const images = '../build/images/';
+const css = '../build/css/';
+const js = '../build/js/';
 
-    console.log(`Deleting directory "${path}"...`);
-    fs.rmdirSync(path);
+const publicImages = '../src/public/images/';
+const publicCss = '../src/public/css/';
+const publicJs = '../src/public/js/';
+
+
+fs.readdirSync(images).forEach((file) => {
+  try {
+    // console.log(images + file);
+    fs.unlinkSync(images + file);
+  } catch(err) {
+    console.log(err);
   }
-};
+});
 
-console.log("Cleaning working tree...");
+fs.rmdirSync(images);
 
-deleteFolderRecursive("./build");
 
-console.log("Successfully cleaned working tree!");
+
+fs.readdirSync(css).forEach((file) => {
+  try {
+    // console.log(css + file);
+    fs.unlinkSync(css + file);
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+fs.rmdirSync(css);
+
+
+fs.readdirSync(js).forEach((file) => {
+  try {
+    // console.log(css + file);
+    fs.unlinkSync(js + file);
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+fs.rmdirSync(js);
+
+fs.readdirSync(build).forEach((file) => {
+  try {
+    // console.log(build + file);
+    fs.unlinkSync(build + file);
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+// these two need to go after I delete the clutter in the main build folder
+ fs.mkdirSync(images);
+ fs.mkdirSync(css);
+ fs.mkdirSync(js);
+
+
+ fs.readdirSync(publicImages).forEach(file => {
+   try {
+     fs.copyFileSync('../src/public/images/' + file, '../build/images/' + file);
+   } catch(err) {
+     console.log(err);
+   }
+ });
+
+ fs.readdirSync(publicCss).forEach(file => {
+   try {
+     fs.copyFileSync('../src/public/css/' + file, '../build/css/' + file);
+   } catch(err) {
+     console.log(err);
+   }
+ });
+
+
+  fs.readdirSync(publicJs).forEach(file => {
+    try {
+      fs.copyFileSync('../src/public/js/' + file, '../build/js/' + file);
+    } catch(err) {
+      console.log(err);
+    }
+  });
