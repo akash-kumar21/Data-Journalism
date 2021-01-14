cf865480709ffe4cea0a9fb5ba6c298edeb73a6f
Akash Kumar <akashkumar@Akashs-MacBook-Pro.local>
2021-01-14 16:41:26 -0500

    ordered data.json by fixing conversion.js and fixed micro.ejs to include 5 (instead of all) fields in the graphs

--- conversion.js
+++ conversion.js
@@ -9,13 +9,17 @@ let lines = spreadsheet_csv.split("\n");
 
 
 
-for (let i = 5-1; i <= 15-1; i++) {
+for (let i = 5-1; i <= 15-1; i++) {//loops through race
   if (i != 6-1) {
+    let allFields = []; // [ [field1, years], [field2, years], ... ]
+
     let initialLine = lines[i];
     let initialInfo = initialLine.split(',');
     let race = {};
     race['name'] = initialInfo[0];
-    for (let j = i+13; j <= 470; j += 13) {
+    for (let j = i+13; j <= 458; j += 13) {//loops through fields; exclude non-S&E
+      let fieldArray = [];
+
       let raceLine = lines[j];
       let raceInfo = raceLine.split(',');
       let fieldLine = lines[j - i + 2];
@@ -29,11 +33,26 @@ for (let i = 5-1; i <= 15-1; i++) {
       }
       let averageYears = Math.round((sumYears)/11);
       yearArray.push(averageYears);
-      race[fieldName] = yearArray;
+
+      fieldArray.push(fieldName);
+      fieldArray.push(yearArray);
+      allFields.push(fieldArray);
+    }// j loop
+
+    allFields.sort(function(arr1, arr2) {
+      return (arr2[1][11] - arr1[1][11]);
+    });
+
+    for (let j = 0; j < allFields.length; j++) {
+      let specificField = allFields[j];
+      race[specificField[0]] = specificField[1];
     }
+
     races.push(race);
-  }
-}
+  }// if statement
+}// i loop
+
+
 
 
 fs.writeFileSync('data/data.json', JSON.stringify(races), 'utf8');
