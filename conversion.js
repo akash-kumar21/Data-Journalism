50fae4d75753c2b24845a1c8b0b16b4368aa750d
Akash Kumar <akashkumar@Akashs-MacBook-Pro.local>
2021-01-17 19:39:38 -0500

    included 'other' in graphs and added css

--- conversion.js
+++ conversion.js
@@ -17,7 +17,10 @@ for (let i = 5-1; i <= 15-1; i++) {//loops through race
     let initialInfo = initialLine.split(',');
     let race = {};
     race['name'] = initialInfo[0];
-    for (let j = i+13; j <= 458; j += 13) {//loops through fields; exclude non-S&E
+    for (let j = i+13; j <= 458 && j != 327; j += 13) {//loops through fields; exclude non-S&E
+
+
+
       let fieldArray = [];
 
       let raceLine = lines[j];
@@ -25,18 +28,21 @@ for (let i = 5-1; i <= 15-1; i++) {//loops through race
       let fieldLine = lines[j - i + 2];
       let fieldInfo = fieldLine.split(',');
       let fieldName = fieldInfo[0];
-      let yearArray = [];
-      let sumYears = 0;
-      for (let k = 1; k <= 11; k++) {
-        sumYears += 1*raceInfo[k];
-        yearArray.push(1*raceInfo[k]);
-      }
-      let averageYears = Math.round((sumYears)/11);
-      yearArray.push(averageYears);
 
-      fieldArray.push(fieldName);
-      fieldArray.push(yearArray);
-      allFields.push(fieldArray);
+      if (fieldName != "Other" && fieldName != "Other ") {
+        let yearArray = [];
+        let sumYears = 0;
+        for (let k = 1; k <= 11; k++) {
+          sumYears += 1*raceInfo[k];
+          yearArray.push(1*raceInfo[k]);
+        }
+        let averageYears = Math.round((sumYears)/11);
+        yearArray.push(averageYears);
+
+        fieldArray.push(fieldName);
+        fieldArray.push(yearArray);
+        allFields.push(fieldArray);
+      }//if fieldName != 'Other'
     }// j loop
 
     allFields.sort(function(arr1, arr2) {
